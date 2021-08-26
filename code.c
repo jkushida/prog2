b07cfd09d00bff9b938ac7e7ca00ab1fb2110877
Jun-ichi Kushida <>
2021-08-26 20:14:12 +0900

    構造体まで

--- code.c
+++ code.c
@@ -1,21 +1,17 @@
-
-// マクロ名定義を使ったコード
+// 構造体の使用例
 #include <stdio.h>
 
-#define SIZE 5
+struct student{      // 学生情報
+  int no;                   // 学生番号
+  char name[10];   // 氏名
+  double average;  // 平均点
+};
 
-int main() {
-    int a[SIZE];
-    int i;
-    
-    for (i = 0; i < SIZE ; i++)
-        a[i] = SIZE * i;
-    
-    
-    for (i = 0; i < SIZE ; i++)
-        printf("%d ", a[i]);
-    
-    printf("\n");
-    
+int main()
+{
+    struct student seito1 = {5, "Yamada", 85.3};
+
+    // 構造体の持つ情報を表示
+    printf("学生番号: %d,  氏名: %s,  平均点: %f\n",seito1.no, seito1.name, seito1.average);
     return 0;
 }
