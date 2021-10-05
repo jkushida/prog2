7ba5995fc8a5ac9258d38265d3c0d0f242e2c5bf
Jun-ichi Kushida <>
2021-10-05 23:21:33 +0900

    no message

--- code.c
+++ code.c
@@ -1,28 +1,29 @@
 #include <stdio.h>
 
-// 関数外で枠型の宣言
-struct student{    // 学生情報
-    int no;          // 学生番号
-    char name[10];   // 氏名
-    double average;  // 平均点
-};
-
-void func();
-
-int main(){
+int main(void)
+{
+    char str1[] = "abcde";
+    char str2[6] = {0};
+    char *p1, *p2;
     
-    // main関数内で宣言と初期化
-    struct student seito1 = {5, "Yamada", 85.3};
-    printf("学生番号: %d,  氏名: %s,  平均点: %f\n",seito1.no, seito1.name, seito1.average);
+    p1 = str1;
+    p2 = str2;
+    while(*p1!='\0'){
+        p1++;
+    }
+    p1--;
     
-    func();
+    while(*p1!='a'){
+        *p2 = *p1;
+        p2++;
+        p1--;
+    }
+    *p2 = *p1;
+    
+    
+    p2 = str2;
+    
+    printf("%s\n",p2);
     
     return 0;
-}
-
-void func(){
-    
-    // func関数内で宣言と初期化
-    struct student seito1 = {8, "Tanaka", 34.8};
-    printf("学生番号: %d,  氏名: %s,  平均点: %f\n",seito1.no, seito1.name, seito1.average);
 }
