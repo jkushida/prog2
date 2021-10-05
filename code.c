c100cdca8e1e3fbc4f2e20e0179d0609e99103b9
Jun-ichi Kushida <>
2021-10-05 10:08:30 +0900

    no message

--- code.c
+++ code.c
@@ -1,64 +1,28 @@
-// d3-4.c 解答
 #include <stdio.h>
-#include <string.h>
 
-typedef struct{ // 学生情報
-    int no;
-    char name[8];
-    int score[3];
-    double average;
-    char hantei[10];
-    
-}student_dt;
+// 関数外で枠型の宣言
+struct student{    // 学生情報
+    int no;          // 学生番号
+    char name[10];   // 氏名
+    double average;  // 平均点
+};
 
-void print_one_data(student_dt sp);
-void seiseki_syori(student_dt *sp);
-
-#define N 5
+void func();
 
 int main(){
-    int i;
-    student_dt info[N] = {
-        {101, "Sakata", {90, 60, 65}, 0, "-"},
-        {102, "Tanaka", {56, 65, 78}, 0, "-"},
-        {103, "Sasaki", {92, 89, 58}, 0, "-"},
-        {104, "Yamada", {35, 45, 67}, 0, "-"},
-        {105, "harada", {23, 87, 36}, 0, "-"},
-    };
-    printf("成績処理前\n");
-    for(i=0; i<N; i++)
-        print_one_data(info[i]);
     
-    // 配列score[3]の要素の平均からhanteiを決定する.
-    for(i=0; i<N; i++)
-        seiseki_syori(&info[i]);
+    // main関数内で宣言と初期化
+    struct student seito1 = {5, "Yamada", 85.3};
+    printf("学生番号: %d,  氏名: %s,  平均点: %f\n",seito1.no, seito1.name, seito1.average);
     
-    printf("成績処理後\n");
-    for(i=0; i<N; i++)
-        print_one_data(info[i]);
+    func();
+    
     return 0;
 }
 
-// 一人分のデータを表示する関数
-void print_one_data(student_dt sp){
-    int i;
-    printf("%s ", sp.name);
+void func(){
     
-    for(i=0;i<3;i++)
-        printf("%d ", sp.score[i]);
-    
-    printf("%.1f ", sp.average);
-    printf("%s \n", sp.hantei);
-}
-
-void seiseki_syori(student_dt *sp){
-    int i;
-    double sum=0;
-    for(i=0;i<3;i++)
-        sum += sp->score[i];
-    sp->average=sum/3.0;
-    if(sp->average >= 60)
-        strcpy(sp->hantei, "合格");
-    else
-        strcpy(sp->hantei, "不合格");
+    // func関数内で宣言と初期化
+    struct student seito1 = {8, "Tanaka", 34.8};
+    printf("学生番号: %d,  氏名: %s,  平均点: %f\n",seito1.no, seito1.name, seito1.average);
 }
