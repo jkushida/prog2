00148f5317bd0808fcd0102f9d3dd6c984ac186a
Jun-ichi Kushida <>
2021-10-12 16:43:49 +0900

    no message

--- code.c
+++ code.c
@@ -1,29 +1,53 @@
+// e1-7.c 解答
 #include <stdio.h>
+
+struct  member{
+    int     no;          // 番号
+    char    name[10];    // 氏名
+    int     age;         // 年齢
+    int     exp;         // 勤務年数
+};
+
+void disp(struct member *p);
+
+char *fname = "meibo2.csv";
 
 int main(void)
 {
-    char str1[] = "abcde";
-    char str2[6] = {0};
-    char *p1, *p2;
+    int i;
+    FILE *fp;
+    struct  member p[5]={
+        {1, "田中", 21, 4},
+        {2, "山田", 19, 3},
+        {3, "坂本", 20, 5},
+        {4, "岡本", 30, 10},
+        {4, "佐藤", 50, 18}
+    };
     
-    p1 = str1;
-    p2 = str2;
-    while(*p1!='\0'){
-        p1++;
+    // 最初ファイルを書き込み専用で開き，閉じることで中身をクリアする
+    fp = fopen(fname,"w");
+    fclose(fp);
+
+    for(i=0;i<5;i++){
+        disp(&p[i]);
     }
-    p1--;
-    
-    while(*p1!='a'){
-        *p2 = *p1;
-        p2++;
-        p1--;
-    }
-    *p2 = *p1;
-    
-    
-    p2 = str2;
-    
-    printf("%s\n",p2);
     
     return 0;
+}
+
+void disp(struct member *p)
+{
+    FILE *fp;
+    int i;
+    
+    /* 追加書き込みでファイルを開く*/
+    /* コードを書く*/
+    fp = fopen(fname,"a");
+    
+    /* データ一件分を表示 */
+    /* コードを書く*/
+    fprintf(fp, "%d, %s, %d, %d\n",p->no, p->name, p->age, p->exp);
+    
+    printf("一件分のデータを書き込み\n");
+    fclose(fp);
 }
